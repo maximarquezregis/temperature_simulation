24e3aec82de63ab9661489eccb46bc49ef1c2dfc
maximarquezregis <maximarquez2004@gmail.com>
2025-06-05 19:35:13 -0300

    Added main method to read parameters and show results

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import simulation_methods
+from simulation_methods import euler
+
 
 def read_parameters(filename):
     parameters = {}
@@ -11,8 +12,27 @@ def read_parameters(filename):
 
     return parameters
 
-file = 'parameters.txt'
-parameters = read_parameters(file)
 
-for key, value in parameters.items():
-    print(f"{key}: {value}")
+if __name__ == "__main__":
+
+    file = 'parameters.txt'
+    parameters = read_parameters(file)
+
+    for key, value in parameters.items():
+        print(f"{key}: {value}")
+
+    # Convert string parameters to appropriate types
+    t_amb = float(parameters["t_amb"])
+    k = float(parameters["k"])
+    q = float(parameters["q"])
+    turn_on = float(parameters["t_on"])
+    turn_off = float(parameters["t_off"])
+    num_steps = int(parameters["sim_time"])
+    initial_temp = float(parameters["t_0"])
+    h = float(parameters["h"])
+
+    result = euler(t_amb, k, q, turn_on, turn_off, num_steps, initial_temp, h)
+
+    # Print the result temperatures
+    for i, temp in enumerate(result):
+        print(f"Step {i}: {temp}")
